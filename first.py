722c88d3c041bd2e084df62d77c1c040144336a0
Abdelrahman-95 <abdomoh.7e4@gmail.com>
2022-05-17 22:49:00 +0200

    Secound edit

--- first.py
+++ first.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     print("connection started")
     while True:
         board.digital[7].write(1)
-        time.sleep(1)
+        time.sleep(2)
         board.digital[7].write(0)
-        time.sleep(1)
+        time.sleep(2)
